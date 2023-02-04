ad3c6cf53cc106870ea15c81d2153dc3961392e2
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-04 11:43:09 +0100

    Add NewsCardComponent

---
+++ src/app/components/news-card/news-card.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsCardComponent } from './news-card.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [NewsCardComponent],
+  imports: [
+    CommonModule, IonicModule],
+  exports: [NewsCardComponent]
+
+})
+export class NewsCardModule { }
