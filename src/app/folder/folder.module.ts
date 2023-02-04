3a7373f36683b14d9a8f52b7ddafce8c03120557
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-04 11:56:56 +0100

    Ion card with photos

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { NewsCardComponent } from '../components/news-card/news-card.component';
+import { NewsCardModule } from '../components/news-card/news-card.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    NewsCardModule,
   ],
   declarations: [FolderPage]
 })
