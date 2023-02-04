b51ca45f84306117302f2c9e0177238cce46623e
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-04 12:14:06 +0100

    Make connection and interface

---
+++ src/app/services/newsapi.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsapiService {
+  topHeadlinesPath = environment.baseURL;
+
+  constructor(private http:HttpClient) { }
+
+  getTopHeadlines(country: string, category: string){
+    return this.http.get(this.topHeadlinesPath+`?country=${country}&category=${category}
+    &pageSize=10&apiKey=${environment.apiKey}`)
+  }
+}
