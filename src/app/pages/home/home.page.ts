b1fc16725034c84545072f87987084f7a988b507
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-05 00:22:56 +0100

    Change home.page.html

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/interfaces/person';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  person:Person ={name:'',email:''}
+  themeName:string='';
+  public personName:string="";
+  public personEmail:string="";
+  
+
+  constructor(private storage:StorageService) { }
+
+  setStorage()
+  {
+
+  }
+  async getStorage()
+  {
+
+  }
+  async updateStorage()
+  {
+
+  }
+  async clearStorage()
+  {
+
+  }
+
+  async deleteFromStorage()
+  {
+    
+  }
 
   ngOnInit() {
   }
